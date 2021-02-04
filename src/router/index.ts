542717d8c0a3c84e8b35151f71f929cde34f915f
William <19631981+willy00@users.noreply.github.com>
2021-02-04 17:50:20 +0800

    Progress check 1. Committing changes #3

--- src/router/index.ts
+++ src/router/index.ts
@@ -24,6 +24,21 @@ const routes: Array<RouteConfig> = [
     component: () => import(/* webpackChunkName: "password-manager" */ '../views/PasswordManager/AddPassword.vue'),
   },
   {
+    path: '/password-manager/credit-cards',
+    name: 'Credit Cards',
+    component: () => import(/* webpackChunkName: "password-manager" */ '../views/PasswordManager/CreditCards.vue'),
+  },
+  {
+    path: '/password-manager/password-generator',
+    name: 'Password Generator',
+    component: () => import(/* webpackChunkName: "password-manager" */ '../views/PasswordManager/PasswordGenerator.vue'),
+  },
+  {
+    path: '/password-manager/change-master-pass',
+    name: 'Change Master Password',
+    component: () => import(/* webpackChunkName: "password-manager" */ '../views/PasswordManager/ChangeMasterPassword.vue'),
+  },
+  {
     path: '/software-updater',
     name: 'Software Updater',
     component: () => import(/* webpackChunkName: "software-updater" */ '../views/SoftwareUpdater/SoftwareUpdater.vue'),
@@ -32,6 +47,36 @@ const routes: Array<RouteConfig> = [
     path: '/anti-virus',
     name: 'Anti Virus',
     component: () => import(/* webpackChunkName: "anti-virus" */ '../views/AntiVirus/AntiVirus.vue'),
+  },
+  {
+    path: '/anti-virus/scheduled-scan',
+    name: 'Scheduled Scan',
+    component: () => import(/* webpackChunkName: "anti-virus" */ '../views/AntiVirus/ScheduledScan.vue'),
+  },
+  {
+    path: '/backup',
+    name: 'Backup',
+    component: () => import(/* webpackChunkName: "backup" */ '../views/Backup/Backup.vue'),
+  },
+  {
+    path: '/backup/upload-files',
+    name: 'Upload Files',
+    component: () => import(/* webpackChunkName: "backup" */ '../views/Backup/Uploadfiles.vue'),
+  },
+  {
+    path: '/backup/scheduled-backup-settings',
+    name: 'Scheduled Backup Settings',
+    component: () => import(/* webpackChunkName: "backup" */ '../views/Backup/ScheduledBackupSettings.vue'),
+  },
+  {
+    path: '/email-filtering',
+    name: 'Email Filtering',
+    component: () => import(/* webpackChunkName: "email-filtering" */ '../views/EmailFiltering/EmailFilter.vue'),
+  },
+  {
+    path: '/email-filtering/whitelist-management',
+    name: 'Whitelist Management',
+    component: () => import(/* webpackChunkName: "email-filtering" */ '../views/EmailFiltering/WhitelistManagement.vue'),
   },
 ];
 
